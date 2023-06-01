4b009534432f66a92bd9a7c61e77512ed9cc5954
Aniv Surana <aniv.surana@gmail.com>
2023-05-31 21:53:38 -0500

    Fix broken links

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -6,14 +6,14 @@ const ProjectCardData = [
         imgsrc: pro1,
         title: "Star Catalog Multithreading",
         text: "Aims to improve the performance of calculating the average angular distance between stars by implementing multi-threading using pthreads and determining the optimal number of threads, while ensuring correctness and documenting the results in a report.",
-        view: "https://www.youtube.com/midtrac",
-        source: "https://www.github.com/anivsurana"
+        view: "https://github.com/anivsurana/TychoStarCatalog",
+        source: "https://github.com/anivsurana/TychoStarCatalog"
     },
     {
         imgsrc: pro2,
         title: "MemoirMate",
         text: "An Android application designed using Android Studio, XML and Java. Family and friends travel diary used to store memories for family and friends to look back at.",
-        view: "https://www.youtube.com/watch?v=6jJt6fb7KJk",
+        view: "https://github.com/anivsurana/MemoirMate",
         source: "https://github.com/anivsurana/MemoirMate"
     },
     {
