2ad15b2845cf8599022f8f91cdcb73a1812563b3
Aniv Surana <aniv.surana@gmail.com>
2023-05-31 21:07:47 -0500

    Update images

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,4 +1,4 @@
-import pro1 from "../assets/project1.png"
+import pro1 from "../assets/project1.jpeg"
 import pro2 from "../assets/project2.png"
 import pro3 from "../assets/project3.png"
 const ProjectCardData = [
